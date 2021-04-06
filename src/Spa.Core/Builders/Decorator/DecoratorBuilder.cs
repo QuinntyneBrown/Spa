fcc6c6b9d8aa12e7a08820c16de037d0df2ba5c5
QuinntyneBrown <quinntynebrown@gmail.com>
2021-04-06 04:29:01 -0400

    update

--- src/Spa.Core/Builders/Decorator/DecoratorBuilder.cs
+++ src/Spa.Core/Builders/Decorator/DecoratorBuilder.cs
@@ -1,9 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using Spa.Core.Services;
+using System.Collections.Generic;
 
 namespace Spa.Core.Builders
 {
     public class DecoratorBuilder
     {
+        private readonly string _name;
+        private IFileSystem _fileSystem;
+        private ICommandService _commandService;
+
+        public DecoratorBuilder(IFileSystem fileSystem, ICommandService commandService, string name)
+        {
+            _name = name;
+        }
         public string[] Content
         {
             get
