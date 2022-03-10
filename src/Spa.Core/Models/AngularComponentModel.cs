c4536eb9f14efc69cd04e6d5afa081c876c5ba96
Quinntyne Brown <quinntynebrown@gmail.com>
2022-03-10 17:23:54 -0500

    update

---
+++ src/Spa.Core/Models/AngularComponentModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Spa.Core.Models
+{
+    public class AngularComponentModel
+    {
+        public FileModel TypeScriptFile { get; set; }
+        public FileModel ScssFile { get; set; }
+        public FileModel HtmlFile { get; set; }
+        public string Name { get; set; }
+
+        public AngularComponentModel(string name)
+        {
+            Name = name;
+        }
+
+        public AngularComponentModel()
+        {
+
+        }
+    }
+}
